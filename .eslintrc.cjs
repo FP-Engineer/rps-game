249223c3bdbf7ba1692e40cf6f910016b247aecc
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-06-17 22:43:38 +0200

    chore: formatted eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,13 +9,20 @@ module.exports = {
 		browser: true,
 		es2020: true
 	},
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended',
+		'plugin:storybook/recommended',
+	],
 	parser: '@typescript-eslint/parser',
 	parserOptions: {
 		ecmaVersion: 'latest',
 		sourceType: 'module'
 	},
-	plugins: ['react-refresh'],
+	plugins: [
+		'react-refresh',
+	],
 	rules: {
 		'react-refresh/only-export-components': 'warn',
 		'indent': [rules.ON, 'tab']
