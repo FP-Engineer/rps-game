4709bfcb1f2f078638e0046ff5bfed2fcabc036d
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-06-17 16:41:45 +0200

    chore: setup storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,23 @@
 const rules = {
 	ON: 2,
 	OFF: 0,
-	WARN: 1,
+	WARN: 1
 };
 
 module.exports = {
-	env: { browser: true, es2020: true },
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:react-hooks/recommended',
-	],
+	env: {
+		browser: true,
+		es2020: true
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "plugin:storybook/recommended"],
 	parser: '@typescript-eslint/parser',
-	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
 	plugins: ['react-refresh'],
 	rules: {
 		'react-refresh/only-export-components': 'warn',
 		'indent': [rules.ON, 'tab']
-	},
-}
+	}
+};
