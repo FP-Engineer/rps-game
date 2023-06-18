ea954d55b0d8030aa89aa2c0a992d3bc51225894
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-06-18 22:15:41 +0200

    chore: fixed test setup

---
+++ jest.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+	preset: 'ts-jest/presets/js-with-ts',
+	testMatch: [
+		'<rootDir>/src/**/*.spec.tsx',
+	],
+	coveragePathIgnorePatterns: [
+		'/node_modules/',
+		'dist/',
+		'main.tsx',
+		'<rootDir>/src/types/',
+	],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.{js,ts,tsx,jsx}',
+		'!<rootDir>/src/**/*.stories.*'
+	],
+	coverageThreshold: {
+		global: {
+			branches: 90,
+			functions: 90,
+			lines: 90,
+			statements: 90,
+		},
+	},
+	testEnvironment: 'jsdom',
+	transform: {
+		'\\.css\\.ts$': '@vanilla-extract/jest-transform',
+	}
+};
