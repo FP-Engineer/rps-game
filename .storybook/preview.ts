cc8c3c1eb01a8c5216645bf687ca8af12af2b4dd
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-06-28 21:51:15 +0200

    fix(showcase): removed padding from preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,15 +1,17 @@
+import 'normalize.css';
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
-  parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
-    },
-  },
+	parameters: {
+		actions: { argTypesRegex: "^on[A-Z].*" },
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
+		},
+		layout: 'fullscreen',
+	},
 };
 
 export default preview;
