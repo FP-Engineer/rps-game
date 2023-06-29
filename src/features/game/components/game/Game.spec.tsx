805bf6d21bc7bb948c7ec5878b7f860ede155787
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-06-29 23:51:31 +0200

    feat(feature): implemented basic game rules

---
+++ src/features/game/components/game/Game.spec.tsx
@@ -0,0 +1,19 @@
+import { render, getAllByRole } from '@testing-library/react';
+import { Game } from './Game';
+import { rules } from '../../constants';
+
+describe('Game Test Suite', () => {
+
+	it('renders without crashing.', () => {
+
+		render(<Game rules={[]} />);
+	});
+
+	it('presents all possible choices', () => {
+
+		const { container } = render(<Game rules={rules} />);
+		const buttons = getAllByRole(container, 'button');
+
+		expect(buttons.length).toBe(rules.length);
+	});
+});
