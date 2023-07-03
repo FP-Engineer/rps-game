efb414a855fa3afbfd28436f26d24975f4e89d07
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-07-03 23:38:23 +0200

    refactor: refactored game component

--- src/features/game/components/game/Game.tsx
+++ src/features/game/components/game/Game.tsx
@@ -4,6 +4,9 @@ import { Rule } from '../../types/Rule';
 import { Choice } from '../../types/Choice';
 import { container } from './Game.css';
 import { useEvaluation } from '../../models/evaluation/Evaluation';
+import { usePlayer } from '../../models/player/Player';
+import { PlayerTurn } from '../player-turn';
+import { Announcement } from '../announcement';
 
 export interface Props {
 	rules: Array<Rule>,
@@ -13,24 +16,16 @@ export const Game: React.FC<Props> = ({ rules }) => {
 
 	const options: Array<Choice> = useMemo(() => rules.map(({choice}) => choice), [rules]);
 	const { evaluate } = useEvaluation(rules);
-	const choose = (playerChoice: Choice) => {
-
-		const computerChoice = options[Math.floor(Math.random() * options.length)];
-		const result = evaluate(playerChoice, computerChoice);
-
-		alert(`You: ${playerChoice} Computer: ${computerChoice} Result: ${result}`);
-	}
+	const { choice: playerChoice, choose } = usePlayer();
+	const computerChoice = options[Math.floor(Math.random() * options.length)];
+	const result = evaluate(playerChoice, computerChoice);
 
 	return(
 		<div className={ container }>
-			{options.map((choice) => (
-				<button
-					key={choice}
-					onClick={() => { choose(choice) }}
-				>
-					{choice}
-				</button>
-			))}
+			{playerChoice === Choice.initial
+				? <PlayerTurn options={options} choose={choose} />
+				: <Announcement playerOneChoice={playerChoice} playerTwoChoice={computerChoice} result={result} />
+			}
 		</div>
 	);
 };
