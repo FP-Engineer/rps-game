7ccb5cd0e1a5906bfef3ad6b28fe633bc6572ebd
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-08-02 22:19:24 +0200

    chore: configured vite to handle svg imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,6 @@ export default defineConfig({
 	plugins: [
 		react(),
 		vanillaExtractPlugin(),
+		svgr(),
 	],
 });
