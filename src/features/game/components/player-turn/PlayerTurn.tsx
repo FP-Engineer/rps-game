b925aa5654694581ed851cd0e7f8683500c1fd8f
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-08-02 22:21:28 +0200

    feat(component): integrated scissors component

--- src/features/game/components/player-turn/PlayerTurn.tsx
+++ src/features/game/components/player-turn/PlayerTurn.tsx
@@ -1,22 +1,28 @@
+import React from 'react';
 import { Choice } from '../../types';
 import { container } from './PlayerTurn.css';
+import { Scissors } from '../scissors';
+import { Option } from '../option';
 
 export interface Props {
 	options: Array<Choice>;
 	choose: (choice: Choice) => void;
 }
 
+const optionsMap = new Map([
+	[Choice.scissors, Scissors]
+]);
+
 export const PlayerTurn: React.FC<Props> = ({ options, choose }) => {
 
 	return <div className={ container }>
-		{options.map((choice) => (
-			<button
-				key={choice}
-				onClick={() => { choose(choice) }}
-			>
-				{choice}
-			</button>
-		))}
+		{options.map((choice) => {
+			const Component = optionsMap.get(choice);
+
+			return Component
+				? <Component handleClick={choose} />
+				: <Option label={''} handleClick={() => choose(choice)}  />
+		})}
 	</div>;
 };
 
