5b458eee3ecb833fc9c20a77c27d31bfcbc0c386
Fabian Pfeiffer <fabian_pfeiffer@gmx.net>
2023-08-03 22:25:03 +0200

    feat(component): implemented label component

---
+++ src/components/label/Label.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+	display: 'inline-block',
+	fontFamily: '"Barlow Semi Condensed", sans-serif',
+	fontWeight: '600',
+	color: 'white',
+});
+
+export const bold = style({
+	fontWeight: '700',
+});
